17569571b5b5345775b25dbd55ba6049e3845030
Gabriel Fischer Abati <39131927+git-gfischer@users.noreply.github.com>
2022-05-29 00:40:41 -0300

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y ffmpeg
 #From opencv documentation
 sudo apt-get install -y build-essential
 sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-dev python3-dev libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y python-dev python3-dev libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev libboost-all-dev
 
 #I recommand install numpy via pip
 python3 -m pip install numpy
